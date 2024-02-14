8cc0dc8b6f7fe2e1c224d226f1fd492361c4b3ee
Korben Dallas <Kirillklimov1488@gmail.com>
2024-02-15 01:38:43 +0300

    gotovo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// задача 1
+// Задайте значение М и N
+// Напишите прогу, которая выведет все натур числа в промежутке от М до N
+// Использовать рекурсию, не использовать циклы
+
+
+Console.Write("Введите значение M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N:");
+int n = Convert.ToInt32(Console.ReadLine());
+void PrintNaturalNumbers(int m, int n)
+{
+if (m > n)
+return;
+
+Console.Write($"{m}, ");
+
+PrintNaturalNumbers(m+1,n);
+
+}
+PrintNaturalNumbers(m,n);
